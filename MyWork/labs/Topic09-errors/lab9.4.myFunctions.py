d726d8b674ca375b74d263c2d58781938075eb01
Ryan <G00398318@gmit.ie>
2021-03-27 22:44:40 +0000

    Weekly Lab sheet 09

---
+++ MyWork/labs/Topic09-errors/lab9.4.myFunctions.py
@@ -0,0 +1,20 @@
+# lab9.4.myFunctions.py
+# Author David
+
+def fibonacci(number):
+    return[]
+
+
+
+return7 = [0,1,1,2,3,5,8]
+return11 = [0,1,1,2,3,5,8,13,21,34,55]
+assert fibonacci(7) == return7, 'incorrect return for 7'
+assert fibonacci(11) == return11, 'incorrect return for 7'
+assert fibonacci(0) == [], 'incorrect return for 7'
+assert fibonacci(1) == [0], 'incorrect return for 7'
+
+
+if __name__ == '__main__':
+    # test will go here
+    print("all good")
+
