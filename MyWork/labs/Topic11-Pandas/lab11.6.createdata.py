e09b2ce0588596d3e6c0064585ba12a56ea26c27
Ryan <G00398318@gmit.ie>
2021-03-28 22:03:02 +0100

    Weekly Lab Sheet 11

---
+++ MyWork/labs/Topic11-Pandas/lab11.6.createdata.py
@@ -0,0 +1,41 @@
+# lab11.5.createdata.py
+# Author David
+
+
+import pandas as pd
+
+listdata = [
+    ['John', 'maths',       23],
+    ['John', 'programming', 66],
+    ['Mary', 'maths',       45],
+    ['Mary', 'programming', 33],
+    ['Mark', 'maths',       57],
+    ['Mark', 'programming', 70],
+    ['Pete', 'maths',       73],
+    ['Pete', 'programming', 61]
+]
+
+
+df = pd.DataFrame(listdata, columns=['name','subject','grade'])
+print(df.head(3))
+
+print(df.describe())
+print(type(df.describe()))
+
+
+# Write file to csv
+csvfilename = 'grades.csv'
+df.to_csv(csvfilename)
+
+# Write an excel file
+excelfilename = 'grades.xlsx'
+df.to_excel(excelfilename, index=False, sheet_name='data')
+with pd.ExcelWriter(excelfilename, engine='openpyxl', mode='a') as writer:
+    df.describe().to_excel(writer, sheet_name="summary")
+
+# Calculte and print the mean of the grades
+mean = df.describe().loc['mean','grade']
+print(mean)
+# or 
+mean = df['grade'].mean()
+print(mean)
