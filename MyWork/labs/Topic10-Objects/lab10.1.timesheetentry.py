3280ed6cb6656b78c28d080620545744f5390fe0
Ryan <G00398318@gmit.ie>
2021-03-28 16:18:13 +0100

    Weekly Lab Sheet 10

---
+++ MyWork/labs/Topic10-Objects/lab10.1.timesheetentry.py
@@ -0,0 +1,10 @@
+# lab10.1.timesheetentry.py
+# Author David
+
+class Timesheetentry:
+
+    def __init__(self, project, start, duration):
+        self.project = project
+        self.start = start
+        self.duration = duration
+    
---
+++ MyWork/labs/Topic10-Objects/lab10.2.timesheetentry.py
@@ -0,0 +1,12 @@
+# lab10.1.timesheetentry.py
+# Author David
+
+class Timesheetentry:
+
+    def __init__(self, project, start, duration):
+        self.project = project
+        self.start = start
+        self.duration = duration
+    
+    def __str__(self):
+        return self.project + ':' + str(self.duration)
